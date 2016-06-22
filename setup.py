2dd9232cd95f9c482876fad2fce9815b21f89a6c
Sigurd Næss <sigurdkn@gmail.com>
2016-06-22 15:38:53 +0100

    Ok, put things in correct manifest file. Hopefully this will work.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.extension import Extension
 
 setup(
 	name="sharp",
-	version="1.0.1",
+	version="1.0.2",
 	author="Sigurd Naess",
 	author_email="sigurdkn@astro.uio.no",
 	description=("Wrapper for the libsharp spherical harmonics transofrm library"),
 	long_description=("A python wrapper of the libsharp spherical harmonics "
 		"transform library, from before libsharp got its own wrapper. This "
 		"version used to be more general, but that may no longer be the case."),
-	download_url = 'https://github.com/amaurea/sharp/tarball/1.0.1',
+	download_url = 'https://github.com/amaurea/sharp/tarball/1.0.2',
 	license = "CC0",
 	keywords = "science spherical harmonics transform",
 	url = "http://packages.python.org/sharp",
