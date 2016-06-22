9bcd6f5d624bdb5ca88862062949584d3ee4feb4
Sigurd Næss <sigurdkn@gmail.com>
2016-06-22 15:08:50 +0100

    Added download_url

--- setup.py
+++ setup.py
@@ -3,17 +3,21 @@ from distutils.extension import Extension
 
 setup(
 	name="sharp",
+	packages=["sharp"],
 	version="1.0.0",
 	author="Sigurd Naess",
 	author_email="sigurdkn@astro.uio.no",
-	description=("A python wrapper of the libsharp spherical harmonics "
+	description=("Wrapper for the libsharp spherical harmonics transofrm library"),
+	long_description=("A python wrapper of the libsharp spherical harmonics "
 		"transform library, from before libsharp got its own wrapper. This "
 		"version used to be more general, but that may no longer be the case."),
+	download_url = 'https://github.com/amaurea/sharp/tarball/1.0.0',
 	license = "CC0",
 	keywords = "science spherical harmonics transform",
 	url = "http://packages.python.org/sharp",
 	classifiers=[
 		"Topic :: Scientific/Engineering",
+		"License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
 	],
 	ext_modules = [
 		Extension(
